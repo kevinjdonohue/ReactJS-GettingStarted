8c8f6474b6a160709206657963100ee16570232b
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-09-24 17:06:06 -0700

    Cleaned up code and added comments

--- getting-started-app/src/App.js
+++ getting-started-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import Form from './form';
 import CardList from './cardlist';
 
@@ -16,7 +15,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div id="gettting-started-app">        
         <Form onSubmit={this.addNewCard} />
         <CardList cards={this.state.cards} />
       </div>
--- getting-started-app/src/form.js
+++ getting-started-app/src/form.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-class Form extends React.Component {
+class Form extends Component {
     state = {userName: ''}
 
     handleSubmit = (event) => {
         event.preventDefault();        
         axios.get(`https://api.github.com/users/${this.state.userName}`)
             .then(resp => {               
-                this.props.onSubmit(resp.data);
-                this.setState({userName: ''});
+                this.props.onSubmit(resp.data); //the onSubmit function here is the addNewCard function that was pased in by the App component
+                this.setState({userName: ''});  //this clears out the local state's userName variable after the new card is added
             });
     };
 
     render() {
+        // overall: this renders an HTML form with an onSubmit handler with a textbox with an onChange handler
+        // the onSubmit handler invokes the local handleSubmit function
+        // the onChange handler sets the local state's userName variable to the entered value 
         return (
             <form onSubmit={this.handleSubmit}>
                 <input type="text" 
