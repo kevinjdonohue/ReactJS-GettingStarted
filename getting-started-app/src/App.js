e65d871118a68200df1df923c14b54fd62e180d2
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-09-24 22:14:15 -0700

    Added a componentDidMount method to pre-load some cards; added some utility methods to handle ajax call

--- getting-started-app/src/App.js
+++ getting-started-app/src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import Form from './form';
 import CardList from './cardlist';
+import axios from 'axios';
 
 class App extends Component {
   state = {
     cards: []
   };
+
+  componentDidMount() {
+    this.getCard("kevinjdonohue");    
+    this.getCard("devdaves");
+    this.getCard("insikcho");
+  }
+
+  getCard(userName) {
+    axios.get(`http://api.github.com/users/${userName}`)
+    .then(response => {
+      this.handleResponse(response);
+    })
+    .catch(error => {
+      this.handleError(error);
+    });
+  }
+
+  handleResponse(response) {
+    this.setState(prevState => ({
+      cards: prevState.cards.concat(response.data)
+    }));
+  }
+
+  handleError(error) {
+    console.log("An error occurred: " + error.response.status + " - " + error.response.statusText + " - " + error.response.request.responseURL);
+  }
 
   addNewCard = (cardInfo) => {
     this.setState(prevState => ({
@@ -15,7 +42,7 @@ class App extends Component {
 
   render() {
     return (
-      <div id="getting-started-app">        
+      <div id="getting-started-app">
         <Form onSubmit={this.addNewCard} />
         <CardList cards={this.state.cards} />
       </div>
