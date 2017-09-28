2a1ebd5b37f87d2501953a40fed3682b2c1a02e2
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-09-27 22:34:27 -0700

    Added some styles and updated numbers and stars to be rendered dynamically

--- stars-app/src/numbers.js
+++ stars-app/src/numbers.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 const Numbers = (props) => {
+    const arrayOfNumbers = Array.from(Array(10).keys());
+    arrayOfNumbers.map(x => x++);
+
     return (
         <div className="card text-center">
             <div>
                 <span>1</span>
-                <span>2</span>
-                <span>3</span>
+                <span className="selected">2</span>
+                <span className="used">3</span>
             </div>
         </div>
     );
--- stars-app/src/stars.js
+++ stars-app/src/stars.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 const Stars = (props) => {
+    const numberOfStars = 1 + Math.floor(Math.random()*9);
+
+    let stars = [];
+    for (let i = 0; i < numberOfStars; i++) {
+        stars.push(<i key={i} className="fa fa-star fa-2x"></i>)
+    }
+
     return (
         <div className="col-sm-4 text-center">
-            <i className="fa fa-star fa-2x"></i>
-            <i className="fa fa-star fa-2x"></i>
-            <i className="fa fa-star fa-2x"></i>
-            <i className="fa fa-star fa-2x"></i>
-            <i className="fa fa-star fa-2x"></i>
+            {stars}
         </div>
     );
 }
